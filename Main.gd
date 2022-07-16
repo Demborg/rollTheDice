6acc0a3b0d197e24d57f2a6453876d2ab255680b
Axel Demborg <axel.demborg@gmail.com>
2022-07-17 00:47:04 +0200

    Make it controlable by mouse

--- Main.gd
+++ Main.gd
@@ -7,7 +7,7 @@ func _ready():
 func _on_Player_dead():
 	$UserInterface/Retry.show()
 	
-func _unhandled_input(event):
+func _input(event):
 	if event.is_action("ui_accept") and ($UserInterface/Retry.visible or $UserInterface/Win.visible):
 		get_tree().reload_current_scene()
 
--- Player.gd
+++ Player.gd
@@ -8,9 +8,24 @@ var DURATION = 0.5
 var direction = Vector3.ZERO
 var progress = 0
 
+var click = Vector2.ZERO
+
 func _ready():
 	for i in range($Pivot/MeshInstance.get_child_count()):
 		$Pivot/MeshInstance.get_child(i).set_value(i)
+		
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			click = event.position
+		else:
+			var diff = click - event.position
+			if diff.length() > 100:
+				if abs(diff.x) > 2 * abs(diff.y):
+					direction = Vector3.LEFT * sign(diff.x) * 2
+				elif abs(diff.y) > 2 * abs(diff.x):
+					direction = Vector3.FORWARD * sign(diff.y) * 2
+			click = Vector2.ZERO
 
 func _physics_process(delta):
 	move_and_collide((direction + 1 * Vector3.DOWN) * delta / DURATION)
