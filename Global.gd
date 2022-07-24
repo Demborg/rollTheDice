03799aaee289dc3981b9269be191b63541c0697d
Axel Demborg <axel.demborg@gmail.com>
2022-07-24 15:00:13 +0200

    Read seed from url if available

--- Global.gd
+++ Global.gd
@@ -5,7 +5,20 @@ var _rng = RandomNumberGenerator.new()
 var global_seed: int
 var color: Color
 
+func _load_state():
+	if OS.has_feature('JavaScript'):
+		return JavaScript.eval(""" 
+				var url_string = window.location.href;
+				var url = new URL(url_string);
+				url.searchParams.get('seed');
+			""")
+	return null
+
 func _ready():
-	_rng.randomize()
-	global_seed = _rng.randi()
+	var loaded_seed = _load_state()
+	if loaded_seed:
+		_rng.seed = int(loaded_seed)
+	else:
+		_rng.randomize()
+	global_seed = _rng.seed
 	color = Color.from_hsv(_rng.randf(), 1, 1)
--- ShareButton.gd
+++ ShareButton.gd
@@ -3,3 +3,4 @@ extends Button
 
 func _on_ShareButton_pressed():
 	OS.set_clipboard("demb.org/rollTheDice?seed=%s" % Global.global_seed)
+	text = "Copied"
