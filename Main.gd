c66c51504f51c344b10cc7c7e2b38f06c153e35b
Axel Demborg <axel.demborg@gmail.com>
2022-08-08 20:47:49 +0200

    don't stack targets

--- Main.gd
+++ Main.gd
@@ -3,12 +3,14 @@ extends Node
 export (PackedScene) var target_scene
 
 var rng = RandomNumberGenerator.new()
+var targets: Dictionary
 
 func _reset():
 	get_tree().reload_current_scene()
 
 func _make_and_connect_target(position: Vector2, value: int):
 	var target = target_scene.instance()
+	targets[position] = value
 	target.value = value
 	target.transform.origin = Vector3(position.x, 1, position.y)
 	target.connect("hit", $UserInterface/Progress, "_on_Target_hit")
@@ -16,17 +18,16 @@ func _make_and_connect_target(position: Vector2, value: int):
 	$Ground.add_child(target)
 	
 func _make_and_connect_random_target():
-	_make_and_connect_target(
-		Vector2(
-			2 * rng.randi_range(-2, 2),
-			2 * rng.randi_range(-2, 2)
-		),
-		rng.randi_range(1, 6)
-	)
+	var val = rng.randi_range(1, 6)
+	var pos = 2 * Vector2(rng.randi_range(-2, 2), rng.randi_range(-2, 2))
+	while pos in targets.keys():
+		pos = 2 * Vector2(rng._randi_range(-2, 2), rng.randi_range(-2, 2))
+	_make_and_connect_target(pos, val)
 
 func _ready():
 	$DirectionalLight.light_color = Global.color
 	rng.seed = Global.level + Global.global_seed
+	targets = {}
 	$UserInterface/Retry.hide()
 	$UserInterface/Win.hide()
 	
