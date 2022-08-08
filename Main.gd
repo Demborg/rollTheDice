54121ecf999cf99caec0ecf5535c45d774d63587
Axel Demborg <axel.demborg@gmail.com>
2022-08-08 20:33:49 +0200

    target position as vec2

--- Main.gd
+++ Main.gd
@@ -7,19 +7,18 @@ var rng = RandomNumberGenerator.new()
 func _reset():
 	get_tree().reload_current_scene()
 
-func _make_and_connect_target(position: Vector3, value: int):
+func _make_and_connect_target(position: Vector2, value: int):
 	var target = target_scene.instance()
 	target.value = value
-	target.transform.origin = position
+	target.transform.origin = Vector3(position.x, 1, position.y)
 	target.connect("hit", $UserInterface/Progress, "_on_Target_hit")
 	$UserInterface/Progress.targets += 1
 	$Ground.add_child(target)
 	
 func _make_and_connect_random_target():
 	_make_and_connect_target(
-		Vector3(
+		Vector2(
 			2 * rng.randi_range(-2, 2),
-			1,
 			2 * rng.randi_range(-2, 2)
 		),
 		rng.randi_range(1, 6)
@@ -32,8 +31,8 @@ func _ready():
 	$UserInterface/Win.hide()
 	
 	if Global.level == 0:
-		_make_and_connect_target(Vector3(0, 1, 0), 3)
-		_make_and_connect_target(Vector3(0, 1, 4), 1)
+		_make_and_connect_target(Vector2(0, 0), 3)
+		_make_and_connect_target(Vector2(0, 4), 1)
 	else:
 		for _i in range(Global.level):
 			_make_and_connect_random_target()
